eb057f034ff022983826986dc3573f7db51a5dda
Pranit Harekar <pranitharekar@hotmail.com>
2017-06-15 23:45:25 -0400

    Create permPalindrome.swift

---
+++ permPalindrome.swift
@@ -0,0 +1,21 @@
+
+//  Created by Pranit  Harekar on 6/15/17.
+//  Copyright © 2017 Pranit Harekar. All rights reserved.
+
+// Write an efficient function that checks whether any permutation of an input string is a palindrome
+
+    
+    func permPalindrome(_ str: String) -> Bool{
+        var charset = Set<Character>()
+        
+        for c in str.characters{
+            if charset.contains(c){
+                charset.remove(c)
+            }else{
+                charset.insert(c)
+            }
+        }
+        
+        return charset.count <= 1
+        
+    }
