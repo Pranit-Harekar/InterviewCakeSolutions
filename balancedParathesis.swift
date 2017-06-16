e32e70e7514ed1826c38432f86e177bb3685d119
Pranit Harekar <pranitharekar@hotmail.com>
2017-06-15 23:29:39 -0400

    Create balancedParathesis.swift

---
+++ balancedParathesis.swift
@@ -0,0 +1,26 @@
+
+//  Created by Pranit  Harekar on 6/15/17.
+//  Copyright © 2017 Pranit Harekar. All rights reserved.
+
+// Write a function to check if the parathesis are balanced
+
+func balancedParathesis(_ str : String) -> Bool{
+        
+        let charSet = Array(str.characters)
+        var buffer = [Int]()
+        for c in charSet{
+            if c == "("{
+               buffer.append(charSet.index(of: c)!)
+            }
+            if c == ")" {
+                if !buffer.isEmpty{
+                    buffer.removeLast()
+                }else{
+                    return false
+                }
+            }
+
+        }
+        return buffer.isEmpty ? true : false
+        
+    }
